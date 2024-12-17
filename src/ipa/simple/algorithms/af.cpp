af1d9990bdee29be10a239fd7a417923ed50fe1a
Iwan <77576646+CookieLover3000@users.noreply.github.com>
2024-12-17 14:22:13 +0100

    reformated af.cpp

--- src/ipa/simple/algorithms/af.cpp
+++ src/ipa/simple/algorithms/af.cpp
@@ -6,66 +6,91 @@ namespace libcamera {
 
 namespace ipa::soft::algorithms {
 
-Af::Af() : lensPos(0) {}
+Af::Af()
+	: lensPos(0)
+{
+}
 
 void Af::process([[maybe_unused]] IPAContext &context, [[maybe_unused]] const uint32_t frame,
-                [[maybe_unused]] IPAFrameContext &frameContext, [[maybe_unused]] const SwIspStats *stats,
-                [[maybe_unused]] ControlList &metadata) {
-    switch (context.activeState.af.state) {
-        case 3:     // Full Sweep
-            if (lensPos < 1023) { // TODO: CHANGE 255 TO DYNAMIC VALUE
-                values[lensPos] = stats->sharpnessValue_;
-                context.activeState.af.sharpnessLock = values[lensPos];
-                context.activeState.af.focus = lensPos;
-                lensPos++;
-            } else {
-                std::pair<uint8_t, uint64_t> highest;
-                std::map<uint8_t, uint64_t>::iterator currentEntry;
-                for (currentEntry = values.begin(); currentEntry != values.end(); ++currentEntry) {
-                    if (currentEntry->second > highest.second) {
-                        highest = std::make_pair(currentEntry->first, currentEntry->second);
-                        context.activeState.af.focus = highest.first;
-                        context.activeState.af.sharpnessLock = highest.second;
-                        sharpnessLock = highest.second;
-                    }
-                }
-                context.activeState.af.state = 2;
-            }
-            break;
-        case 1:     // small sweep (hill climb)
-            if (itt < 100) {
-                values[lensPos] = stats->sharpnessValue_;
-                context.activeState.af.sharpnessLock = values[lensPos];
-                context.activeState.af.focus = lensPos++;
-                itt++;
-            } else {
-                context.activeState.af.state = 2;
-            }
-            break;
-        case 2:     //Locked
-            if (sharpnessLock*0.6 > stats->sharpnessValue_) {   // to smallsweep
-                lensPos = 0;
-                context.activeState.af.focus = lensPos;
-                context.activeState.af.state = 3;
-            } else if (sharpnessLock*0.8 > stats->sharpnessValue_) {    // to sweep
-                if (lensPos < 50) { lensPos = 0; }
-                else lensPos = lensPos - 50;
-                context.activeState.af.focus = lensPos;
-                itt = 0;
-                context.activeState.af.state = 1;
-            }
-            break;
-        case 0:
-            if(itt < 100) {
-                itt++;
-            }
-            else {
-                itt = 0;
-                context.activeState.af.state = 3;
-            }
-            break;
-    }
-};
+		 [[maybe_unused]] IPAFrameContext &frameContext, [[maybe_unused]] const SwIspStats *stats,
+		 [[maybe_unused]] ControlList &metadata)
+{
+	switch (context.activeState.af.state) {
+	case 0:
+		initState(context);
+		break;
+	case 1: //Locked
+		lockedState(context, stats);
+		break;
+	case 2: // Full Sweep
+		fullSweepState(context, stats);
+		break;
+	case 3: // small sweep (hill climb)
+        smallSweepState(context, stats);
+		break;
+	}
+}
+
+void Af::initState([[maybe_unused]] IPAContext &context)
+{
+	if (itt < 255) {
+		itt++;
+	} else {
+		itt = 0;
+		context.activeState.af.state = 2; // full sweep
+	}
+}
+
+void Af::lockedState([[maybe_unused]] IPAContext &context, [[maybe_unused]] const SwIspStats *stats)
+{
+	if (sharpnessLock * 0.6 > stats->sharpnessValue_) { // to sweep
+		lensPos = 0;
+		context.activeState.af.focus = lensPos;
+		context.activeState.af.state = 2; // full sweep
+	} else if (sharpnessLock * 0.8 > stats->sharpnessValue_) { // to smallsweep
+		if (lensPos < 50) {
+			lensPos = 0;
+		} else
+			lensPos = lensPos - 50;
+		context.activeState.af.focus = lensPos;
+		itt = 0;
+		context.activeState.af.state = 3; // small sweep
+	}
+}
+
+void Af::fullSweepState([[maybe_unused]] IPAContext &context, [[maybe_unused]] const SwIspStats *stats)
+{
+	if (lensPos < 1023) { // TODO: CHANGE TO DYNAMIC VALUE
+		values[lensPos] = stats->sharpnessValue_;
+		context.activeState.af.sharpnessLock = values[lensPos];
+		context.activeState.af.focus = lensPos;
+		lensPos++;
+	} else {
+		std::pair<uint8_t, uint64_t> highest;
+		std::map<uint8_t, uint64_t>::iterator currentEntry;
+		for (currentEntry = values.begin(); currentEntry != values.end(); ++currentEntry) {
+			if (currentEntry->second > highest.second) {
+				highest = std::make_pair(currentEntry->first, currentEntry->second);
+				context.activeState.af.focus = highest.first;
+				context.activeState.af.sharpnessLock = highest.second;
+				sharpnessLock = highest.second;
+			}
+		}
+		context.activeState.af.state = 1; // locked
+	}
+}
+
+void Af::smallSweepState([[maybe_unused]] IPAContext &context, [[maybe_unused]] const SwIspStats *stats)
+{
+	if (itt < 100) {
+		values[lensPos] = stats->sharpnessValue_;
+		context.activeState.af.sharpnessLock = values[lensPos];
+		context.activeState.af.focus = lensPos++;
+		itt++;
+	} else {
+		context.activeState.af.state = 1; // locked
+	}
+}
 
 REGISTER_IPA_ALGORITHM(Af, "Af")
 
--- src/ipa/simple/algorithms/af.h
+++ src/ipa/simple/algorithms/af.h
@@ -7,21 +7,26 @@ namespace libcamera {
 namespace ipa::soft::algorithms {
 
 class Af : public Algorithm
-{   
+{
 public:
-    Af();
-    ~Af() = default;
+	Af();
+	~Af() = default;
 
-    void process(IPAContext &context, const uint32_t frame,
-            IPAFrameContext &frameContext,
-		    const SwIspStats *stats,
-		    ControlList &metadata) override;
+	void process(IPAContext &context, const uint32_t frame,
+		     IPAFrameContext &frameContext,
+		     const SwIspStats *stats,
+		     ControlList &metadata) override;
 
 private:
-    uint32_t lensPos;
-    std::map<uint8_t,uint64_t> values;
-    uint64_t sharpnessLock;
-    uint8_t itt;
+	void fullSweepState(IPAContext &context, const SwIspStats *stats);
+	void smallSweepState(IPAContext &context, const SwIspStats *stats);
+	void lockedState(IPAContext &context, const SwIspStats *stats);
+	void initState(IPAContext &context);
+
+	uint32_t lensPos;
+	std::map<uint8_t, uint64_t> values;
+	uint64_t sharpnessLock;
+	uint8_t itt;
 };
 
 } /* namespace ipa::soft::algorithms */
